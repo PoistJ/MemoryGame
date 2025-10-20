02517cbaf2911118153209a4e9e1a44bbefb049d
PoistJ <157406020+PoistJ@users.noreply.github.com>
2025-10-20 03:31:11 -0400

    Unfortunately didn't commit during initial development. Created skeleton structure, setup fetch API to get pokemon data from PokeAPI. Setup correct logic to increment score and track highscore.

---
+++ MemoryGame/src/App.jsx
@@ -0,0 +1,57 @@
+import "./App.css";
+import { useState } from "react";
+import { Card } from "./components/components.jsx";
+
+function App() {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  const shuffledArr = shuffle(arr);
+  const [clicked, setClicked] = useState([]);
+  const [highscore, setHighscore] = useState(0);
+  const currentlyClicked = clicked;
+
+  const handleClickEvent = (event) => {
+    const val = event.target.className;
+
+    if (!clicked.find((id) => id === val)) {
+      setClicked((currentlyClicked) => [...currentlyClicked, val]);
+    } else {
+      if (highscore < clicked.length) {
+        setHighscore(clicked.length);
+      }
+      setClicked([]);
+    }
+  };
+
+  return (
+    <>
+      <header>
+        <div className="scoreboard">
+          <div className="currentScore">Current score: {clicked.length}</div>
+          <div className="highscore">Highscore: {highscore}</div>
+        </div>
+      </header>
+      {shuffledArr.map((value) => {
+        return (
+          <Card key={value} arrIndex={value} handleClick={handleClickEvent} />
+        );
+      })}
+    </>
+  );
+}
+
+function shuffle(array) {
+  var i = array.length,
+    j = 0,
+    temp;
+  while (i--) {
+    j = Math.floor(Math.random() * (i + 1));
+
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  return array;
+}
+
+export default App;
---
+++ MemoryGame/src/components/components.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+
+const pokemon = [
+  { id: 1, fetchID: "onix" },
+  { id: 2, fetchID: "pikachu" },
+  { id: 3, fetchID: "jigglypuff" },
+  { id: 4, fetchID: "torchic" },
+  { id: 5, fetchID: "bulbasaur" },
+  { id: 6, fetchID: "charizard" },
+  { id: 7, fetchID: "dragonite" },
+  { id: 8, fetchID: "piplup" },
+  { id: 9, fetchID: "bidoof" },
+  { id: 10, fetchID: "excadrill" },
+  { id: 11, fetchID: "jynx" },
+  { id: 12, fetchID: "diglett" },
+];
+
+const options = {
+  method: "GET",
+};
+
+function Card({ arrIndex, handleClick }) {
+  const pokeID = pokemon.find(({ id }) => id === arrIndex).fetchID;
+  const [pokeName, setPokeName] = useState("");
+  const [imgURL, setImgURL] = useState(null);
+
+  useEffect(() => {
+    const getPoke = async (lookupID) => {
+      const url = `https://pokeapi.co/api/v2/pokemon/${lookupID}`;
+      const response = await fetch(url, options);
+      const data = await response.json();
+      setPokeName(data.name);
+      setImgURL(data.sprites.front_default);
+    };
+    getPoke(pokeID);
+  }, [pokeID]);
+
+  return (
+    <div key={arrIndex} id={arrIndex} onClick={handleClick}>
+      <img src={imgURL} className={arrIndex} />
+      <div className={arrIndex}>
+        {pokeName}
+      </div>
+    </div>
+  );
+}
+
+export { Card };
